58ff258b2cf0557738e09e7903e44cf2714702cb
Evan Lloyd <elloyd@patientslikeme.com>
2018-09-28 09:25:51 -0400

    Move audio logic back into service

--- src/lib/GuitarChord/components/MusicButton.js
+++ src/lib/GuitarChord/components/MusicButton.js
@@ -1,54 +1,22 @@
 import React from 'react';
-import WebAudioFontPlayer from 'webaudiofont';
 
-import guitarAudio from '../services/guitarAudio';
 import { playChord, fretsToMidi } from '../services/audioService';
 
-class MusicButton extends React.Component {
-  componentDidMount() {
-    // player.loaded.decodeAfterLoading expects this to be on window;
-    window.guitarAudio = guitarAudio;
-    const AudioContextFunc = window.AudioContext || window.webkitAudioContext;
-    this.audioContext = new AudioContextFunc();
-    const player = new WebAudioFontPlayer();
-    player.loader.decodeAfterLoading(this.audioContext, 'guitarAudio');
-    this.player = player;
-  }
-
-  handleOnClick = () => {
-    const notes = fretsToMidi(this.props.frets);
-
-
-    // support this.props.strum?
-    // this.player.queueStrum(this.audioContext, this.audioContext.destination, guitarAudio, 0, notes, 1.5)
-
-    // arpeggiate notes
-    notes.forEach((note, index) => {
-      index++;
-      setTimeout(() => {
-        this.player.queueWaveTable(
-          this.audioContext,
-          this.audioContext.destination,
-          guitarAudio,
-          0,
-          note,
-          0.5
-        );
-      }, index * 500);
-    });
+const MusicButton = ({ frets, buttonClasses, buttonStyles }) => {
+  const handleOnClick = () => {
+    const notes = fretsToMidi(frets);
+    playChord(notes);
   };
 
-  render() {
-    return (
-      <button
-        onClick={this.handleOnClick}
-        className={this.props.buttonClasses}
-        style={this.props.buttonStyles}
-      >
-        Play
-      </button>
-    );
-  }
+  return (
+    <button
+      onClick={handleOnClick}
+      className={buttonClasses}
+      style={buttonStyles}
+    >
+      Play
+    </button>
+  );
 }
 
 export default MusicButton;
--- src/lib/GuitarChord/services/audioService.js
+++ src/lib/GuitarChord/services/audioService.js
@@ -1,4 +1,13 @@
-import Synth from './AudioSynth';
+import WebAudioFontPlayer from 'webaudiofont';
+
+import guitarAudio from './guitarAudio';
+
+// setup because WebAudioFontPlayer expects the synth file to be a global :(
+window.guitarAudio = guitarAudio;
+const AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContextFunc();
+const player = new WebAudioFontPlayer();
+player.loader.decodeAfterLoading(audioContext, 'guitarAudio');
 
 export const fretsToMidi = (frets) => {
   // map frets == ['X', 3, 2, 0, 1, 0]
@@ -12,45 +21,21 @@ export const fretsToMidi = (frets) => {
     return standardTuningMidi[index] + fret;
   });
 
-  return notes.filter((fret) => fret != 'x');
+  return notes.filter((fret) => fret !== 'x');
 };
 
-export const playChord = (frets) => {
-  // frets == ['X', 3, 2, 0, 1, 0]
-  const notes = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
-  // assumes standard tuning
-  const strings = ['E','A','D','G','B','E'];
-  // starting octaves of the open strings
-  const octaves = [2,2,3,3,3,4];
-  const chord = frets;
-
-  const chordNotes = strings.map((stringName, index) => {
-    if (chord[index] === 'x') {
-      return 'x';
-    } else {
-      const startingNote = parseInt(notes.indexOf(stringName), 10);
-      const noteAdjust = parseInt(chord[index], 10);
-      let chordtone = startingNote + noteAdjust;
-      // hack to handle going up an octave
-      // when chordtone is longer than array of notes
-      if(chordtone >= 12) {
-        octaves[index]++;
-        chordtone = (parseInt(chordtone, 10) - 12);
-      }
-      return notes[chordtone];
-    }
-  });
-
-  const acoustic = Synth.createInstrument('acoustic');
-
-  // can't skip over 'x' because then octaves get messed up, instead just don't increament i if a muted note.
-  let i = 0;
-  chordNotes.forEach((value, index) => {
-    if(value !== 'x') {
-      i++;
-      setTimeout(() => {
-        acoustic.play(value, octaves[index], 2);
-      }, i*500);
-    }
+export const playChord = (midiNotes) => {
+  midiNotes.forEach((note, index) => {
+    index++;
+    setTimeout(() => {
+      player.queueWaveTable(
+        audioContext,
+        audioContext.destination,
+        guitarAudio,
+        0,
+        note,
+        0.5
+      );
+    }, index * 500);
   });
 }
