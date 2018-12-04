f63fcf15808b13932c07bdd1b4f318dc444ea4ab
Evan Lloyd <elloyd@patientslikeme.com>
2018-12-04 08:48:40 -0500

    Add tests for rendering Fingering and MusicButton at the top level

--- src/lib/GuitarChord/GuitarChord.test.js
+++ src/lib/GuitarChord/GuitarChord.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import * as Enzyme from 'enzyme';
 
 import GuitarChord from './';
+import Fingering from './components/Fingering';
 import Fretboard from './components/Fretboard';
 import Strings from './components/Strings';
+import MusicButton from './components/MusicButton';
 
 describe('GuitarChord', () => {
   const getWrapper = (overrideProps, renderMethod = 'shallow') => {
@@ -35,6 +37,53 @@ describe('GuitarChord', () => {
 
       const strings = wrapper.find(Strings);
       expect(strings.props().stringCount).toBe(frets.length);
+    });
+  });
+
+  describe('props for Fingering', () => {
+    it('passes the correct props', () => {
+      const props = {
+        fingering: ['x', 'x', 2, 3, 1],
+        frets: ['x', 0, 2, 2, 1, 0],
+      }
+      const wrapper = getWrapper({ ...props });
+
+      const fingeringComponent = wrapper.find(Fingering);
+      const { fingering, frets, lowestFret, fretCount } = fingeringComponent.props();
+
+      expect(fingering).toEqual(props.fingering);
+      expect(frets).toEqual(props.frets);
+      expect(fretCount).toEqual(4);
+      expect(lowestFret).toEqual(0);
+    });
+  });
+
+  describe('MusicButton', () => {
+    it('renders a music button if props.music is true', () => {
+      const wrapper = getWrapper({ music: true });
+
+      const musicButton = wrapper.find(MusicButton);
+
+      expect(musicButton).toHaveLength(1);
+    });
+
+    it('does not render a music button if props.music is false', () => {
+      const wrapper = getWrapper({ music: false });
+
+      const musicButton = wrapper.find(MusicButton);
+
+      expect(musicButton).toHaveLength(0);
+    });
+
+    it('passes props.buttonStyles', () => {
+      const buttonStyles = {
+        color: 'red',
+      };
+      const wrapper = getWrapper({ music: true, buttonStyles });
+
+      const musicButton = wrapper.find(MusicButton);
+
+      expect(musicButton.props().buttonStyles).toMatchObject(buttonStyles);
     });
   });
 });
