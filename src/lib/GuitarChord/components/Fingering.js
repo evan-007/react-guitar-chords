a645b134496623a3f3ef416c37794c81a709d194
Brandon Lum <lumjjb@gmail.com>
2020-05-16 01:07:36 -0400

    Example to add labeling of notes/other labels
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- src/lib/GuitarChord/components/Fingering.js
+++ src/lib/GuitarChord/components/Fingering.js
@@ -7,7 +7,7 @@ const Fingering = ({ frets, fingering, fretCount, lowestFret }) => {
   // numbers in the circles that represent which finger to use
   const svgFingering = [];
   // TODO: this is duplicate code in other render functions
-  const offset =  CONSTANTS.X_WIDTH / 5;
+  const offset = CONSTANTS.X_WIDTH / 5;
   const isOpenChord = frets.includes(0);
   const yOffset = CONSTANTS.Y_MAX / fretCount;
 
@@ -27,13 +27,20 @@ const Fingering = ({ frets, fingering, fretCount, lowestFret }) => {
   frets.forEach((fret, index) => {
     if (fret === 'x' || fret === 0) {
       const xValue = CONSTANTS.X_BASE + (offset * index) - 4; // -1 because of how text sits
+      // If there exists a fingering/label for the open note, use that label
+      let label = fret;
+      if (fret === 0 && fingering) {
+        if (fingering[index] !== "") {
+          label = fingering[index];
+        }
+      }
       return svgFingering.push(
         <text
           key={`zero-fret-${index}`}
           x={xValue}
-          y={CONSTANTS.Y_BASE - 3 }
+          y={CONSTANTS.Y_BASE - 3}
         >
-          {fret}
+          {label}
         </text>
       );
     }
